0c4c31a68ce9c97349c8d4cf6bfb6f3a3eb94fcf
Haydee Rodriguez <ayde.rod@gmail.com>
2023-08-01 16:51:51 -0600

    Upgrade swift to swiftui

---
+++ gasoline/gasolineApp.swift
@@ -0,0 +1,17 @@
+//
+//  gasolineApp.swift
+//  gasoline
+//
+//  Created by Haydee Rodriguez Tinajero on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct gasolineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- gasolineTests/gasolineTests.swift
+++ gasolineTests/gasolineTests.swift
@@ -2,35 +2,35 @@
 //  gasolineTests.swift
 //  gasolineTests
 //
-//  Created by Haydee Rodriguez on 29/10/16.
-//  Copyright © 2016 haydeeTinajero. All rights reserved.
+//  Created by Haydee Rodriguez Tinajero on 01/08/23.
 //
 
 import XCTest
 @testable import gasoline
 
-class gasolineTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
+final class gasolineTests: XCTestCase {
+
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
+
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
-    func testExample() {
+
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-    
-    func testPerformanceExample() {
+
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
